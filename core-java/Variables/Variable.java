d4455a965b30b8e3b17f21d0c15ea289fc1e7586
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-02-13 20:37:35 +0530

    first commit

---
+++ core-java/Variables/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Bro";
+
+        System.out.println("Integer : " + x);
+        System.out.println("Float : " + y);
+        System.out.println("Boolean : " + z);
+        System.out.println("Character : " + symbol);
+        System.out.println("Hello " + name);
+    }
+}
