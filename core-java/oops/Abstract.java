20d5fec429d65a21c49907bfe42cb7ea41d7ca51
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-02-15 16:33:03 +0530

    Abstract

---
+++ core-java/oops/Abstract.java
@@ -0,0 +1,36 @@
+abstract class Laptop {
+    abstract void turnOff();
+
+    void turnOn() {
+        System.out.println("Turnning ON");
+    }
+}
+
+class Asus extends Laptop {
+    @Override
+    public void turnOn() {
+        System.out.println("This is from asus");
+    }
+
+    public void turnOff() {
+        System.out.println("Asus laptop turnning Off");
+    }
+}
+
+class Victor extends Laptop {
+    public void turnOff() {
+        System.out.println("Victor laptop turnning Off");
+    }
+}
+
+public class Abstract {
+    public static void main(String[] args) {
+        Asus asus = new Asus();
+        asus.turnOn();
+        asus.turnOff();
+
+        Victor victor = new Victor();
+        victor.turnOn();
+        victor.turnOff();
+    }
+}
